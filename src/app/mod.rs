47c07897149bed5b419ee4b80b7c9720b12a5d0c
Chris Overcash <covercash2@gmail.com>
2018-07-21 20:57:27 -0700

    work toward async shell

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,6 +2,8 @@ use config::Config;
 use shell::Shell;
 use ui::{Event, Ui};
 
+use std::sync::mpsc;
+
 pub struct App<U: Ui> {
     config: Config,
     shell: Shell,
@@ -18,11 +20,14 @@ impl<U: Ui> App<U> {
     }
 
     pub fn run(mut self) -> Result<(), String> {
+        let mut output = String::new();
+
         'main: loop {
             for event in self.ui.events() {
                 match event {
                     Event::Submit(command) => {
                         eprintln!("submitted: {}", command);
+                        // TODO should not block
                         match self.shell.execute(&command) {
                             Ok(output) => {
                                 let string = String::from_utf8(output.stdout).unwrap();
@@ -40,6 +45,15 @@ impl<U: Ui> App<U> {
                     Event::Exit => return Ok(()),
                 }
             }
+
+            // TODO async?
+            // self.shell
+            //     .copy_output(&mut output)
+            //     .expect("could not copy output to buffer");
+
+            // self.shell
+            //     .copy_output(&mut output_buffer)
+            //     .expect("could not copy output to buffer");
 
             match self.ui.draw() {
                 Ok(()) => (),
--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,17 +1,49 @@
 use std::io;
+use std::io::{Read, Write};
 use std::path::PathBuf;
-use std::process::{Command, Output};
+use std::process::{Child, Command, Output, Stdio};
 
 pub struct Shell {
     bin_path: PathBuf,
+    process: Child,
 }
 
 impl Shell {
     pub fn new(bin_path: PathBuf) -> Shell {
-        return Shell { bin_path: bin_path };
+        let child = Command::new(&bin_path)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("should work dummy");
+        return Shell {
+            bin_path: bin_path,
+            process: child,
+        };
     }
 
-    pub fn execute(&self, command: &str) -> io::Result<Output> {
+    pub fn execute(&mut self, command: &str) -> io::Result<Output> {
+        match self.process.stdin {
+            Some(ref mut stdin) => {
+                stdin
+                    .write_all(command.as_ref())
+                    .expect("could not write to subprocess stdin");
+            }
+            None => eprintln!("couldn't get stdin"),
+        }
         return Command::new(&self.bin_path).arg("-c").arg(command).output();
+    }
+
+    pub fn copy_output(&mut self, buf: &mut String) -> io::Result<usize> {
+        match self.process.stdout {
+            Some(ref mut stdout) => {
+                return stdout.read_to_string(buf);
+            }
+            None => {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "could not get stdout of child process",
+                ));
+            }
+        }
     }
 }
