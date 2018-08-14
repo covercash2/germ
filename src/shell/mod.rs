ff9d2ee4c9b3a46f0b841cb2991ca585b582c7d9
Chris Overcash <covercash2@gmail.com>
2018-08-14 11:19:13 -0700

    added send_stream prototype func

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,10 +1,10 @@
 use std::io;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::iter::Take;
 use std::path::PathBuf;
 use std::process::{ChildStdin, Command, Stdio};
 use std::sync::mpsc::channel;
-use std::sync::mpsc::{Receiver, TryIter};
+use std::sync::mpsc::{Receiver, Sender, TryIter, TryRecvError};
 use std::thread::spawn;
 use std::thread::JoinHandle;
 
@@ -26,13 +26,15 @@ impl Shell {
         let mut child = Command::new(&bin_path)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()
             .expect("should work dummy");
 
         let stdin = child.stdin.take().expect("could not capture stdin");
         let stdout = child.stdout.take().expect("could not capture stdout");
+        let stderr = child.stderr.take().expect("could not capture stderr");
 
-        let (output_tx, output_rx) = channel();
+        let (stdout_tx, stdout_rx) = channel();
 
         // TODO join properly
         let thread_handle = spawn(move || {
@@ -41,16 +43,21 @@ impl Shell {
             // TODO parsing lines is taking a while
             for line in stdout.lines() {
                 match line {
-                    Ok(line) => output_tx.send(line).expect("could not send output"),
+                    Ok(line) => stdout_tx.send(line).expect("could not send output"),
                     Err(ioerr) => eprintln!("warning: could not read line: {}", ioerr),
                 }
             }
         });
 
+        // let stderr_handle = spawn(move || {
+        //     let stderr = BufReader::new(stderr);
+        //     return send_stream(stderr);
+        // });
+
         return Shell {
             child: thread_handle,
             stdin: stdin,
-            stdout: output_rx,
+            stdout: stdout_rx,
         };
     }
 
@@ -70,4 +77,39 @@ impl Shell {
             )
         });
     }
+}
+
+/// continuously buffer output from a stream until
+/// interrupted. then, send output along an output channel.
+fn send_stream<R: Read>(
+    stream: R,
+    sender: Sender<String>,
+    interruptor: Receiver<()>,
+) -> io::Result<()> {
+    let mut stream = BufReader::new(stream);
+
+    let mut buffer = String::new();
+
+    while let Ok(bytes_read) = stream.read_line(&mut buffer) {
+        match interruptor.try_recv() {
+            Ok(()) => {
+                // interrupted
+                // send output
+                sender.send(buffer.clone());
+                buffer.clear();
+            }
+            Err(TryRecvError::Empty) => {
+                // not interrupted
+                // continue to buffer output
+            }
+            Err(TryRecvError::Disconnected) => {
+                return Err(io::Error::new(
+                    io::ErrorKind::ConnectionAborted,
+                    "interruptor channel closed",
+                ));
+            }
+        }
+    }
+
+    return Ok(());
 }
